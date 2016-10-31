68dadd24fcc2873c02ffff4703c45d90a2509623
nick.peleh <nick.peleh@gmail.com>
2016-10-31 16:26:12 +0200

    Add webpack.prod.js config for production

---
+++ config/webpack.prod.js
@@ -0,0 +1,50 @@
+var webpack = require('webpack');
+var webpackMerge = require('webpack-merge');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var autoprefixer = require('autoprefixer');
+var commonConfig = require('./webpack.common.js');
+var helpers = require('./helpers');
+
+const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
+
+module.exports = webpackMerge(commonConfig, {
+  entry: {
+    'polyfills': './src/polyfills.ts',
+    'app': './src/main.ts'
+  },
+
+  devtool: 'source-map',
+
+  output: {
+    path: helpers.root('dist'),
+    publicPath: '/dist/',
+    filename: '[name].[hash].js',
+    chunkFilename: '[id].[hash].chunk.js'
+  },
+
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false,
+      options: {
+        htmlLoader: {
+          minimize: false // workaround for ng2
+        },
+        postcss: [ autoprefixer ]
+      }
+    }),
+    new HtmlWebpackPlugin({
+      template: '!!ejs!src/index.html',
+      rootComponent: '<my-app>Loading...</my-app>'
+    }),
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+    new ExtractTextPlugin('[name].[hash].css'),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'ENV': JSON.stringify(ENV)
+      }
+    })
+  ]
+});
